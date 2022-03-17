620af1c460e61c6b7d9dbe25752e17af2685b336
Jaimin_Vaddoriya <95120545+JaiminV2r@users.noreply.github.com>
2022-03-17 22:57:17 +0530

    connected with mongodb - mongoose

---
+++ mongoose.js
@@ -0,0 +1,17 @@
+//  Connect with mongodb - mongoose                                                            ------------> 17/03/2022 <------------
+var mongoose = require("mongoose")
+var EmpSchema = new mongoose.Schema({_id:Number, eno:Number, ename:String, esal:Number, eadd:String}, {versionKey : false})
+
+var EmpModel = mongoose.model("employees",EmpSchema);
+mongoose.connect("mongodb://localhost/Jaimin");
+var newEmp = new EmpModel({"_id":1, "eno":101, "ename":"jaimin", "esal":4000, "eadd":"surat"})
+newEmp.save(afterDataInserted);
+function afterDataInserted(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("Data Successfully Inserted in Database Jaimin.");
+    }
+    mongoose.disconnect();
+}
