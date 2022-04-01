a375d60b0c8d13576ac9df05eb913a6a720b8c56
Jaimin_Vaddoriya <95120545+JaiminV2r@users.noreply.github.com>
2022-04-01 16:08:03 +0530

    MongoDB Connected with node js
    01/04/2022

--- mongoose.js
+++ mongoose.js
@@ -1,51 +1,81 @@
 const express = require("express");
-const MongoClient = require('mongodb').MongoClient
 const ejs = require("ejs");
 const querystring = require("querystring");
 const app = express();
-const cors= require("cors")
+// const cors = require("cors");
+
+app.use(express.json());
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 var mongoose = require("mongoose");
+const { error } = require("console");
 var EmpSchema = new mongoose.Schema(
   {
-    _id: Number,
     empname: String,
     empsal: Number,
     empadd: String,
     body: String,
   },
-  { versionKey: false }
+  { versionKey: false },
+  { timesTamps: true }
 );
 
 var EmpModel = mongoose.model("Employee", EmpSchema);
-MongoClient.connect("mongodb://localhost/Jaimin");
+mongoose.connect("mongodb://localhost/Jaimin")
+  .then((result) => {
+    console.log("Connected success");
+  })
+  .catch((error) => {
+    console.log("ERor : ", error);
+  });
 
 app.get("/", (req, res) => {
   console.log("No data any here"), res.send("no data in here");
 });
+
 app.get("/create", (req, res) => {
   res.sendFile(__dirname + "/views/insertmongoose.html");
 });
 
 app.get("/insert", (req, res) => {
   res.write("Data in get method");
-  res.end();
-});
-app.post("/insert", (req, res) => {
-  console.log(req.body);
-  var newEmp = new EmpModel(req.body);
+  var newEmp = new EmpModel("req.body",req.query);
 
   newEmp.save(function (err) {
     if (err) {
-      console.log(err);
+      console.log("insert time error : ", err);
+    }
+    else {
+      console.log("Data Successfully Inserted in Database Jaimin.");
+    }
+    // mongoose.disconnect();
+  });
+  res.end();
+});
+
+app.post("/insert", (req, res) => {
+  console.log("req.body 1 1 : :: ::", req.body);
+  var newEmp = new EmpModel(req.body);
+  console.log("newEmp : ",newEmp)
+
+  newEmp.save(function (err) {
+    if (err) {
+      console.log("insert time error : ", err);
     } else {
       console.log("Data Successfully Inserted in Database Jaimin.");
     }
-    mongoose.disconnect();
   });
 });
+
+  app.get("/find",(req,res)=>{
+    EmpModel.find().select("empname").then((data)=>{
+      res.json(data)
+    })
+    .catch((error)=>{
+      console.log("errorroeoer : ",error)
+    })
+  })
 
 const port = process.env.port || 7859;
 app.listen(port, () => {
